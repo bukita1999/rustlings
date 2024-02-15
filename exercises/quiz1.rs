25dc842b8d57df7ad2205422fa5080f2bd360b49
bukita1999 <47375498+bukita1999@users.noreply.github.com>
2024-02-15 06:15:51 +0000

    目前完成内容

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(number:i32) -> i32 {
+    let mut price;
+    if number <= 40 {
+        price = number * 2;
+    }  
+    else{
+        price = number;
+    } 
+    price
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
